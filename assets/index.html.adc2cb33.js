6057582692265dedb524e40cfcda4c55a19dc6b2
haiweilian <haiweilian@foxmail.com>
2022-06-23 11:06:21 +0000

    Deploying to gh-pages from @ bfehub/vmi@42ca0120a2dd4ad5fad4bf821f048fb07b654c57 🚀

--- assets/index.html.adc2cb33.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-138ffd64",path:"/components/npm-badge/",title:"NpmBadge",lang:"en-US",frontmatter:{},excerpt:"",headers:[{level:2,title:"Basic",slug:"basic",children:[]}],git:{updatedTime:1655033391e3,contributors:[{name:"haiweilian",email:"haiweilian@foxmail.com",commits:1}]},filePathRelative:"components/npm-badge/index.md"};export{e as data};
